4e56b4b911c00986c86d49fa1198a11976ceb009
kishu <kishujaiswal007@gmail.com>
2023-04-03 00:55:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+     function func(){
+    console.log('kishu');
+  }
+  {func()}
+  return (
+    <div className="App">
+      
+
+    </div>
+  );
+}
+
+export default App;
